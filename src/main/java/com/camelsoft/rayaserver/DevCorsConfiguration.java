9eef5393663ea60cf978fcdc7e4395284f7ff086
camelsoft <admin@camel-soft.com>
2024-04-25 19:02:30 +0100

    user controller

---
+++ src/main/java/com/camelsoft/rayaserver/DevCorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.camelsoft.rayaserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class DevCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200","https://dashboard.rayafinancing.com","https://meeting.rayafinancing.com","https://rayafinancing.com","https://server.rayafinancing.com")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization");
+    }
+}
