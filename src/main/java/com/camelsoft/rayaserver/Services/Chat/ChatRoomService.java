86cd67cee636737a8405fb8a9ff5a7bf7bd27e84
camelsoft <admin@camel-soft.com>
2024-05-09 18:00:03 +0100

    user controller

--- src/main/java/com/camelsoft/rayaserver/Services/Chat/ChatRoomService.java
+++ src/main/java/com/camelsoft/rayaserver/Services/Chat/ChatRoomService.java
@@ -17,34 +17,34 @@ public class ChatRoomService {
 
     public Optional<String> getChatId(users senderId, users recipientId, String lastMessage, boolean createIfNotExist) {
 
-         return chatRoomRepository
-                .findBySenderAndRecipient(senderId, recipientId)
+        return chatRoomRepository
+                .findBySenderAndRecipient(senderId,recipientId)
                 .map(ChatRoom::getChatId).or(() -> {
                     if(!createIfNotExist) {
                         return  Optional.empty();
                     }
-                     var chatId =
+                    var chatId =
                             String.format("%s_%s", senderId, recipientId);
 
-                     ChatRoom senderRecipient = ChatRoom
-                             .builder()
-                             .chatId(chatId)
-                             .sender(senderId)
-                             .recipient(recipientId)
-                             .lastmessage(lastMessage)
-                             .timestmp(new Date())
-                             .build();
+                    ChatRoom senderRecipient = ChatRoom
+                            .builder()
+                            .chatId(chatId)
+                            .sender(senderId)
+                            .recipient(recipientId)
+                            .lastmessage(lastMessage)
+                            .timestmp(new Date())
+                            .build();
 
-                     ChatRoom recipientSender = ChatRoom
-                             .builder()
-                             .chatId(chatId)
-                             .sender(recipientId)
-                             .recipient(senderId)
-                             .lastmessage(lastMessage)
-                             .timestmp(new Date())
-                             .build();
-                     chatRoomRepository.save(senderRecipient);
-                     chatRoomRepository.save(recipientSender);
+                    ChatRoom recipientSender = ChatRoom
+                            .builder()
+                            .chatId(chatId)
+                            .sender(recipientId)
+                            .recipient(senderId)
+                            .lastmessage(lastMessage)
+                            .timestmp(new Date())
+                            .build();
+                    chatRoomRepository.save(senderRecipient);
+                    chatRoomRepository.save(recipientSender);
 
                     return Optional.of(chatId);
                 });
