688e25edc5b7e9dff107c4cf6da23be15dfaf234
camelsoft <admin@camel-soft.com>
2024-05-09 18:52:27 +0100

    user controller

--- src/main/java/com/camelsoft/rayaserver/Services/Chat/ChatMessageService.java
+++ src/main/java/com/camelsoft/rayaserver/Services/Chat/ChatMessageService.java
@@ -20,7 +20,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -34,19 +33,13 @@ public class ChatMessageService {
     private NotificationServices _notificationservices;
     @Autowired
     private UserService userService;
-    @Autowired
-    private EntityManager entityManager; // Inject EntityManager
     public ChatMessage save(ChatMessage chatMessage) throws InterruptedException {
         chatMessage.setStatus(MessageStatus.RECEIVED);
-        // Reattach detached entity to the persistence context
-        ChatMessage managedChatMessage = entityManager.merge(chatMessage);
-        // Persist the managed entity
-        entityManager.persist(managedChatMessage);
-
+        ChatMessage result = repository.save(chatMessage);
         Notification notificationuser = new Notification(
                 this.userService.findById(chatMessage.getSenderId()),
                 this.userService.findById(chatMessage.getRecipientId())  ,
-                managedChatMessage,
+                result,
                 Action.MESSAGE
 
         );
