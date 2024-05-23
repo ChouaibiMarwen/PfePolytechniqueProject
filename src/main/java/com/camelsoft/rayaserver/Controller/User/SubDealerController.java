3fd2e778319e5b222ea86c24616f8b9df2c1f014
camelsoft-p4 <gallahyosra@gmail.com>
2024-05-23 13:34:13 +0100

    add photo to the supplier add api

--- src/main/java/com/camelsoft/rayaserver/Controller/User/SubDealerController.java
+++ src/main/java/com/camelsoft/rayaserver/Controller/User/SubDealerController.java
@@ -1,5 +1,13 @@
 package com.camelsoft.rayaserver.Controller.User;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/api/v1/sub_dealer")
 public class SubDealerController {
+
+
+
 }
