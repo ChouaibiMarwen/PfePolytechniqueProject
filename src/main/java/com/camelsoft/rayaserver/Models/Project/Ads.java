e0e8e2e4b69bcf59153e542a0430022b2754e604
camelsoft <admin@camel-soft.com>
2024-05-27 17:37:06 +0100

    prod

--- src/main/java/com/camelsoft/rayaserver/Models/Project/Ads.java
+++ src/main/java/com/camelsoft/rayaserver/Models/Project/Ads.java
@@ -1,9 +1,14 @@
 package com.camelsoft.rayaserver.Models.Project;
 
+import com.camelsoft.rayaserver.Models.File.File_model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
@@ -13,4 +18,57 @@ public class Ads  implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
     private Long id;
+    @Column(columnDefinition = "TEXT", name = "url")
+    private String url;
+    @Column(columnDefinition = "TEXT", name = "description")
+    private String description;
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name = "list_ads_id")
+    private Set<File_model> attachments = new HashSet<>();
+    @Column(name = "timestmp")
+    private Date timestmp = new Date();
+
+    public Ads() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<File_model> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(Set<File_model> attachments) {
+        this.attachments = attachments;
+    }
+
+    public Date getTimestmp() {
+        return timestmp;
+    }
+
+    public void setTimestmp(Date timestmp) {
+        this.timestmp = timestmp;
+    }
 }
