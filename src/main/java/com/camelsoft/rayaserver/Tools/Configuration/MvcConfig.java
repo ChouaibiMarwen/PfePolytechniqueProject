c1ddd9b2c9f27bf72d9dbb7b1c3b41a1125d8e7e
camelsoft <admin@camel-soft.com>
2024-05-31 16:46:28 +0100

    prod

--- src/main/java/com/camelsoft/rayaserver/Tools/Configuration/MvcConfig.java
+++ src/main/java/com/camelsoft/rayaserver/Tools/Configuration/MvcConfig.java
@@ -13,21 +13,37 @@ import java.util.concurrent.TimeUnit;
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void configureViewResolvers(final ViewResolverRegistry registry) {
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/src/main/WEB-INF/jsp/", ".jsp");
     }
 
-
-    public @Override void addCorsMappings(CorsRegistry registry) {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","http://localhost:80","https://raya-supplier.camel-soft.com","https://profile.rayafinancing.com","https://development.rayafinancing.com","https://appleid.apple.com","https://admin.rayafinancing.com","https://raya-admin.camel-soft.com","http://localhost:4201","https://dashboard.rayafinancing.com","https://server.rayafinancing.com","https://www.rayafinancing.com","https://admin.camel-soft.com","https://client.camel-soft.com","https://agent.camel-soft.com","https://server.camel-soft.com","https://server-raya.camel-soft.com","https://rayafinancing.com","https://rayafinancing.com")
+                .allowedOrigins(
+                        "http://localhost:4200", "http://localhost:80",
+                        "https://raya-supplier.camel-soft.com",
+                        "https://profile.rayafinancing.com",
+                        "https://development.rayafinancing.com",
+                        "https://appleid.apple.com",
+                        "https://admin.rayafinancing.com",
+                        "https://raya-admin.camel-soft.com",
+                        "http://localhost:4201",
+                        "https://dashboard.rayafinancing.com",
+                        "https://server.rayafinancing.com",
+                        "https://www.rayafinancing.com",
+                        "https://admin.camel-soft.com",
+                        "https://client.camel-soft.com",
+                        "https://agent.camel-soft.com",
+                        "https://server.camel-soft.com",
+                        "https://server-raya.camel-soft.com",
+                        "https://rayafinancing.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         exposeDirectory("WebContent", registry);
         exposeDirectory("/var/lib/jenkins/workspace/server/src/main/resources", registry);
         registry.addResourceHandler("/**").addResourceLocations("");
@@ -39,16 +55,13 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/STATIC/");
         registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
                 .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
-
-
     }
 
     private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
         Path uploadDir = Paths.get(dirName);
         String uploadPath = uploadDir.toFile().getAbsolutePath();
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file://"+ uploadPath + "/").setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+        registry.addResourceHandler("/" + dirName + "/**")
+                .addResourceLocations("file://" + uploadPath + "/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
     }
-
-
-
 }
