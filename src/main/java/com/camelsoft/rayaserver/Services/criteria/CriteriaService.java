2d45c33329f4b72928e25a40f5d359b626b56912
camelsoft-p4 <gallahyosra@gmail.com>
2024-09-13 10:04:41 +0100

    updates 3

--- src/main/java/com/camelsoft/rayaserver/Services/criteria/CriteriaService.java
+++ src/main/java/com/camelsoft/rayaserver/Services/criteria/CriteriaService.java
@@ -234,7 +234,7 @@ public class CriteriaService {
             // Initialize criteria builder and query
             CriteriaQuery<users> query = criteriaBuilder.createQuery(users.class);
             Root<users> user = query.from(users.class);
-            Join<users, PersonalInformation> personalInformationJoin = user.join("personalinformation"); // Assuming you have a PersonalInformation join
+            Join<users, PersonalInformation> personalInformationJoin = user.join("personalinformation", JoinType.LEFT);
 
             // Create a list of predicates
             List<Predicate> predicates = new ArrayList<>();
@@ -302,6 +302,7 @@ public class CriteriaService {
             throw new NotFoundException("No data found");
         }
     }
+
 
 
 
